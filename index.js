150b639a8b29df62efbc0956419c86d3782c2069
m1sht1 <sanj.mins@gmail.com>
2021-10-17 20:07:08 -0400

    Lab 6

---
+++ index.js
@@ -0,0 +1,56 @@
+let express = require('express');
+let mongoose = require('mongoose');
+let Student = require('./models/Student');
+let app = express()
+
+mongoose.connect("mongodb+srv://ss:Gbc1234@cluster0.iksxy.mongodb.net/f2021_comp3123?retryWrites=true&w=majority",
+{
+    useNewUrlParser:true,
+useUnifiedTopology:true
+})
+app.get("/", (req, res)=>
+{
+    res.send("<h1>MongoDB mongoose example")
+})
+//insert new syudent
+app.get("/add", async(req, res)=>
+{
+    let s={
+        first_name: "Sanjana",
+        last_name:"Sharmin",
+        total:100
+    }
+    let new_student = new StudentModel(s)
+    try{
+        await new_student.save(s)
+        console.log("Student Record Saved")
+        res.status(200).send("Student Record Saved")
+    }catch(err){
+        console.log("Error:Student Record Saved" +err)
+        res.status(500).send(err)
+    }
+    res.send("<h1>MongoDB mongoose example")
+})
+app.get("/students", async(req,res)=>{
+    const s= await StudentModel.find({})
+    try{
+        res.send(s)
+    }catch(err){
+        console.log("Error:"+err)
+        res.status(500).send(err)
+    }
+    })
+    app.get("/students", async(req,res)=>{
+        //const s= await StudentModel.find({})
+        //const s= await StudentModel.find({}, "first_name total")
+        const s= await StudentModel.find({}, "first_name total")
+        try{
+            res.send(s)
+        }catch(err){
+            console.log("Error:"+err)
+            res.status(500).send(err)
+        }
+})
+app.listen(8089, ()=>{
+    console.log("server is running at http://localhost:8089")
+})
---
+++ models/Student.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose');
+const StudentSchema = new mongoose.Schema({
+    first_name: String,
+last_name:{
+    type: String,
+    required: true,
+    trim :true,
+    lowercase: true
+},
+total:{
+    type: Number,
+    default:0.0,
+    validate(v){
+        if(v<0) throw new Error(" Total marks can't")
+    }
+    
+}
+})
+const Student = mongoose.model("student", StudentSchema)
+module.exports = Student
